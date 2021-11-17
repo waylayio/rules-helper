f09af9d93c9cd3223856e97262c6f41ae0626cb3
lorenzo van de sijpe <lorenzo.vandesijpe@hotmail.nl>
2021-11-17 15:55:23 +0100

    feat: remove stream validation

--- subflow/builder.js
+++ subflow/builder.js
@@ -1,9 +1,9 @@
-const { isEmpty, last, dropRight, some, map, pick, includes } = require('lodash')
+const { isEmpty, last, dropRight, map, includes } = require('lodash')
 const { checkIfObjectHasRequiredKeys } = require('./helper')
 const { v4: uuid } = require('uuid')
 
 class Builder {
-  constructor (options, SDK) {
+  constructor (options, client) {
     checkIfObjectHasRequiredKeys(['name'])
 
     const { name } = options
@@ -12,52 +12,29 @@ class Builder {
 
     this.steps = []
 
-    this.SDK = SDK
+    this.client = client
   }
 
   addStep (step) {
     checkIfObjectHasRequiredKeys(step, ['name', 'properties'])
     const { name } = step
 
-    const configStep = this.SDK._findStepInConfig(name)
+    const configStep = this.client._findStepInConfig(name)
 
     if (!configStep) throw new Error(`no step setup in config with name ${name}`)
 
-    let lastStep = last(this.steps) || {}
-
     const newStep = { ...step, id: uuid() }
 
-    const isStreamStep = this._isStreamStep(name)
+    const prevStep = last(this.steps)
 
-    if (isStreamStep && !isEmpty(lastStep) && !this._isStreamStep(lastStep.name)) throw new Error('can only add a stream step if the previous step is also a stream step')
-
-    if (!isStreamStep && !isEmpty(lastStep) && this._isStreamStep(lastStep.name)) {
-      const gateStep = {
-        name: 'AND',
-        id: uuid()
-      }
-
-      this.steps = map([...this.steps], step => ({ ...step, target: [gateStep.id] }))
-      this.steps.push(gateStep)
-      // overwrite last step with the newly added gate step
-      lastStep = gateStep
-    }
-
-    if (!isEmpty(lastStep) && !isStreamStep) {
-      // watch out, mutation!
-      lastStep.target = [newStep.id]
-    }
+    // ! mutates !
+    if (!isEmpty(prevStep)) prevStep.target = [newStep.id]
 
     const newSteps = [...this.steps, newStep]
 
-    this.SDK._validateStepProperties(newSteps)
+    this.client._validateStepProperties(newSteps)
 
     this.steps = newSteps
-  }
-
-  _isStreamStep (name) {
-    const configStep = this.SDK._findStepInConfig(name)
-    return some(configStep.plugins, plugin => plugin.dataTrigger === true)
   }
 
   removeStep () {
@@ -80,15 +57,8 @@ class Builder {
     return this.steps
   }
 
-  getPlugins () {
-    return map(this.SDK.builderConfig, config => {
-      const streamStep = this._isStreamStep(config.name)
-      return { ...pick(config, ['name', 'description', 'properties']), stream: streamStep }
-    })
-  }
-
   async createTask () {
-    return this.SDK.createTask({
+    return this.client.createTask({
       name: this.name,
       steps: this.steps
     })
