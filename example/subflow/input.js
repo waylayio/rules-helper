61df9b542c8b3ddb57240bab4a8bea960454eaa3
lorenzo van de sijpe <lorenzo.vandesijpe@hotmail.nl>
2021-11-19 13:32:00 +0100

    chore: update input example

--- example/subflow/input.js
+++ example/subflow/input.js
@@ -5,7 +5,7 @@ module.exports = [
     properties: {
       resource: 'resource1'
     },
-    target: [1]
+    target: [1, 2]
   },
   {
     id: 1,
@@ -16,21 +16,26 @@ module.exports = [
       condition: '>',
       value: '15'
     },
-    target: [2]
+    target: [3]
   },
   {
     id: 2,
-    name: 'truck standing still',
+    name: 'truck engine idle',
     properties: {
       resource: 'resource1',
-      metric: 'engine_status',
-      condition: '==',
-      value: 'true'
+      metric: 'door_open',
+      condition: '>',
+      value: '15'
     },
     target: [3]
   },
   {
     id: 3,
+    name: 'AND',
+    target: [4]
+  },
+  {
+    id: 4,
     name: 'alert message',
     properties: {
       message: 'dummy message'
